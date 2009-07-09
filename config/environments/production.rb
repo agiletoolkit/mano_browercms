06c34aa3412a87f36c5c66bfcabbcd7de1c9da5e
Bob Payne <bobpayne@bob-paynes-macbook-pro-2.local>
2009-07-08 21:44:45 -0400

    updated production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,7 @@ config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
+config.action_controller.page_cache_directory = RAILS_ROOT + "/public/cache/" 
 
 # See everything in the log (default is :info)
 # config.log_level = :debug
