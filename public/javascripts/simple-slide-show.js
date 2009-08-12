218029ca9182fa1d1316830e9dc99f99f55ba06e
Bob Payne <bob@electroglide.biz>
2009-08-12 11:38:40 -0400

    testimonial changes

--- public/javascripts/simple-slide-show.js
+++ public/javascripts/simple-slide-show.js
@@ -1,21 +1,31 @@
 var delay = 3000;
 var start_frame = 0;
+var containerName = 'slides'
+
+
 
 function init() {
-  var slideContainer = $('slide-images');
+  var slideContainer = $(containerName); 
+
   if (!slideContainer) {
     return;
   }
-  
-	var lis = slideContainer.getElementsByTagName('li');
+
+  if (slideContainer.className == 'testimonial') {
+    delay = 10000;
+  }
+
+  var lis = slideContainer.getElementsByTagName('li');
 	
-	for(var i=0; i < lis.length; i++){
-		if(i!=0){
-			lis[i].style.display = 'none';
-		}
+  for(var i=0; i < lis.length; i++){
+	if(i!=0){
+		lis[i].style.display = 'none';
 	}
-	end_frame = lis.length -1;
-	start_slideshow(start_frame, end_frame, delay, lis);	
+  }
+  
+  end_frame = lis.length -1;
+  
+  start_slideshow(start_frame, end_frame, delay, lis);	
 }
 
 function start_slideshow(start_frame, end_frame, delay, lis) {
@@ -24,7 +34,7 @@ function start_slideshow(start_frame, end_frame, delay, lis) {
 
 function fadeInOut(frame, start_frame, end_frame, delay, lis) {
 	return (function() {
-		lis = $('slide-images').getElementsByTagName('li');
+		lis = $(containerName).getElementsByTagName('li');
 		Effect.Fade(lis[frame]);
 		if (frame == end_frame) { frame = start_frame; } else { frame++; }
 		lisAppear = lis[frame];
